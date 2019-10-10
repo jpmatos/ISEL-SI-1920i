6a71d23a13810fd56a97c3807660855abfa9fce9
João Matos <joao.matos.jm@gmail.com>
2019-10-10 22:05:51 +0100

    Done question 8 (MacThenEncrypt)

---
+++ src/MacThenEncrypt.java
@@ -0,0 +1,59 @@
+import javax.crypto.*;
+import java.security.*;
+import java.util.Arrays;
+
+public class MacThenEncrypt {
+
+    private static Key k1;
+    private static Key k2;
+
+    public static void init() throws NoSuchAlgorithmException {
+        KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+        k1 = keyGenerator.generateKey();
+        k2 = keyGenerator.generateKey();
+    }
+
+    public static byte[] encrypt(byte[] message) throws NoSuchAlgorithmException, InvalidKeyException, NoSuchPaddingException, BadPaddingException, IllegalBlockSizeException {
+        //generate MAC mark
+        Mac mac = Mac.getInstance("HmacSHA256");
+        mac.init(k1);
+        byte[] messageMark = mac.doFinal(message);
+
+        //concat with message
+        byte[] messageConcatMark = new byte[message.length + messageMark.length];
+        System.arraycopy(message, 0, messageConcatMark, 0, message.length);
+        System.arraycopy(messageMark, 0, messageConcatMark, message.length, messageMark.length);
+
+        //cipher concat message
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.ENCRYPT_MODE, k2);
+        byte[] res = cipher.doFinal(messageConcatMark);
+
+        return res;
+    }
+
+    public static byte[] decrypt(byte[] message) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
+        //Decipher message
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.DECRYPT_MODE, k2);
+        byte[] messageDecipher = cipher.doFinal(message);
+
+        //Separate message from mark
+        byte[] messageOriginal = new byte[messageDecipher.length - 32];
+        byte[] mark = new byte[32];
+        System.arraycopy(messageDecipher, 0, messageOriginal, 0, messageOriginal.length);
+        System.arraycopy(messageDecipher, messageOriginal.length, mark, 0, mark.length);
+
+        //Verify mark against original message
+        Mac mac = Mac.getInstance("HmacSHA256");
+        mac.init(k1);
+        byte[] messageMark = mac.doFinal(messageOriginal);
+        boolean areEqual = Arrays.equals(messageMark, mark);
+
+        //Return original message if MAC mark matches, return null if not
+        if(areEqual)
+            return messageOriginal;
+        else
+            return null;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,44 @@
+import javax.crypto.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.security.*;
+import java.util.Base64;
+
 public class Main {
-    static void main()
-    {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws NoSuchAlgorithmException, InvalidKeyException, NoSuchPaddingException, BadPaddingException, IllegalBlockSizeException {
+        byte[] message;
+        if(args.length > 0)
+            message = readFile(new File(args[0]));
+        else
+            message = "No file given. Using this message.".getBytes();
+
+        System.out.println(String.format("Original Message: \"%s\"", new String(message)));
+
+        MacThenEncrypt.init();
+
+        byte[] encrypted = MacThenEncrypt.encrypt(message);
+        System.out.println(String.format("Encrypted Message: \"%s\"", new String(encrypted)));
+
+        byte[] decrypted = MacThenEncrypt.decrypt(encrypted);
+        if(decrypted != null) {
+            System.out.println(String.format("Decrypted Message: \"%s\"", new String(decrypted)));
+        }
+        else {
+            System.out.println("MAC did not match. Message was changed.");
+        }
+    }
+    private static byte[] readFile(File file){
+        FileInputStream fis = null;
+        byte[] bArray = new byte[(int) file.length()];
+        try{
+            fis = new FileInputStream(file);
+            fis.read(bArray);
+            fis.close();
+
+        }catch(IOException ioExp){
+            ioExp.printStackTrace();
+        }
+        return bArray;
     }
 }
